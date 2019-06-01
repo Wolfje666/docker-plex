f37caa2c661268568d05ad2ab49500af442c100f
James Eckersall <james.eckersall@gmail.com>
2019-06-01 17:28:20 +0100

    added check option to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+if [ "$1" == "--check" ] || [ "$1" == "-c" ]; then
+  CHECKONLY=true
+fi
+
 URL=$(curl -Ls https://plex.tv/api/downloads/1.json | jq '.computer.Linux.releases[] |select(.distro=="debian" and .build=="linux-x86_64") .url' | sed -e 's/"//g')
 VERSION=$(echo $URL | cut -d\/ -f5)
 
@@ -8,6 +12,13 @@ git pull > /dev/null 2>&1
 DOCKERFILE_VERSION=$(grep "^ARG PLEX_VERSION=" Dockerfile | cut -f2 -d\=)
 
 if [ "${VERSION}" != "${DOCKERFILE_VERSION}" ]; then
+  if [ "${CHECKONLY}" == "true" ]; then
+    echo "There is a new version available"
+    echo "Current version: ${DOCKERFILE_VERSION}"
+    echo "Available version: ${VERSION}"
+    exit 0
+  fi
+
   echo "Updating Dockerfile with version ${VERSION}"
   sed -i -e "s/^\(ARG PLEX_VERSION=\).*$/\1${VERSION}/g" \
          -e "s|^\(ARG PLEX_URL=\).*$|\1${URL}|g" Dockerfile
